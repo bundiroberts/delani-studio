22d0232cfabd308433650f742563bd9f56a23fa7
bundiroberts <bundiroberts@gmail.com>
2021-11-21 20:24:29 +0300

    added toggle

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(".design").click(function() {
+      $("#designpractice-hidden").toggle();
+      $("#designpractice").toggle();
+    });
+  });
+  
+  $(document).ready(function() {
+    $(".development").click(function() {
+      $("#webdevelopment-hidden").toggle();
+      $("#webdevelopment").toggle();
+    });
+  });
+  
+  $(document).ready(function() {
+    $(".product").click(function() {
+      $("#productmanagement-hidden").toggle();
+      $("#productmanagement").toggle();
+    });
+  });
+  
