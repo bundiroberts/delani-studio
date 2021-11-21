00f3732308c8aa77c70f186f6d524dedb150fdaa
bundiroberts <bundiroberts@gmail.com>
2021-11-21 20:32:06 +0300

    removed brackets

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@ $(document).ready(function() {
       $("#designpractice-hidden").toggle();
       $("#designpractice").toggle();
     });
-  });
+    });
   
   $(document).ready(function() {
     $(".development").click(function() {
       $("#webdevelopment-hidden").toggle();
       $("#webdevelopment").toggle();
     });
-  });
+    });
   
   $(document).ready(function() {
     $(".product").click(function() {
       $("#productmanagement-hidden").toggle();
       $("#productmanagement").toggle();
     });
-  });
+    });
   
 
    //hover//
@@ -26,63 +26,42 @@ $(document).ready(function() {
 $(document).ready(function(){
     $('#work1.portfolio').hover(function(){
       $(this).css('opacity', '0.2');
-    // },
-  });
-  });
+    });
+    });
       $(document).ready(function(){
       $('#work2.portfolio').hover(function(){
         $(this).css('opacity', '0.2');
-      // },
-      // function(){
-      //   $(this).css('opacity', '1');
       });
       });
         $(document).ready(function(){
           $('#work3.portfolio').hover(function(){
              $(this).css('opacity', '0.2');
-        // },
-        // function(){
-        //   $(this).css('opacity', '1');
         });
         });
          $(document).ready(function(){
             $('#work4.portfolio').hover(function(){
               $(this).css('opacity', '0.2');
-          // },
-          // function(){
-          //   $(this).css('opacity', '1');
           });
           });
             $(document).ready(function(){
               $('#work5.portfolio').hover(function(){
                 $(this).css('opacity', '0.2');
-            // },
-            // function(){
-            //   $(this).css('opacity', '1');
-            });
-            });
+            
+        });
+        });
             $(document).ready(function(){
               $('#work6.portfolio').hover(function(){
                 $(this).css('opacity', '0.2');
-              // },
-              // function(){
-              //   $(this).css('opacity', '1');
               });
               });
               $(document).ready(function(){
                 $('#work7.portfolio').hover(function(){
                   $(this).css('opacity', '0.2');
-                // },
-                // function(){
-                //   $(this).css('opacity', '1');
                 });
                 });
                $(document).ready(function(){
                   $('#work8.portfolio').hover(function(){
                     $(this).css('opacity', '0.2');
-                  // },
-                  // function(){
-                  //   $(this).css('opacity', '1');
                   });
                   });
 
